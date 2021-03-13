37d3c85086f3996395a3bd5ebeddc9478e6ec3c2
jwang0110 <johnw7@uci.edu>
2021-03-13 01:24:51 -0800

    combine drawer into header

--- src/features/layout/Header.js
+++ src/features/layout/Header.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import Collapse from "@material-ui/core/Collapse";
+import MovieIcon from "@material-ui/icons/Movie";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
@@ -9,28 +19,83 @@ import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
-		flexGrow: 1,
+		display: "flex",
+	},
+	appBar: {
+		zIndex: theme.zIndex.drawer + 1,
 	},
 	menuButton: {
-		marginRight: theme.spacing(2),
+		marginRight: "1rem",
 	},
 	title: {
 		flexGrow: 1,
+	},
+	list: {
+		width: 250,
+	},
+	nested: {
+		paddingLeft: "2rem",
 	},
 }));
 
 export default function ButtonAppBar() {
 	const classes = useStyles();
+	const [openDrawer, setOpenDrawer] = useState(false);
+	const [openMovies, setOpenMovies] = useState(false);
+
+	const toggleDrawer = (open) => () => {
+		setOpenDrawer(open);
+	};
+
+	const MenuList = () => (
+		<div className={classes.list} role="presentation">
+			<List>
+				<ListItem button>
+					<ListItemIcon>
+						<MovieIcon />
+					</ListItemIcon>
+					<ListItemText primary={"movies"} />
+				</ListItem>
+				<ListItem button onClick={() => setOpenMovies(!openMovies)}>
+					<ListItemIcon>
+						<MovieIcon />
+					</ListItemIcon>
+					<ListItemText primary="Movies" />
+					{openMovies ? <ExpandLess /> : <ExpandMore />}
+				</ListItem>
+				<Collapse in={openMovies} timeout="auto" unmountOnExit>
+					<List component="div" disablePadding>
+						<ListItem button className={classes.nested}>
+							<ListItemIcon>
+								<MovieIcon />
+							</ListItemIcon>
+							<ListItemText primary="Top Rated" />
+						</ListItem>
+					</List>
+				</Collapse>
+			</List>
+			<Divider />
+			<List>
+				<ListItem button>
+					<ListItemIcon>
+						<MovieIcon />
+					</ListItemIcon>
+					<ListItemText primary={"logout"} />
+				</ListItem>
+			</List>
+		</div>
+	);
 
 	return (
-		<>
-			<AppBar className={classes.root} position="static">
+		<div className={classes.root}>
+			<AppBar className={classes.appBar} position="fixed">
 				<Toolbar>
 					<IconButton
 						edge="start"
 						className={classes.menuButton}
 						color="inherit"
 						aria-label="menu"
+						onClick={toggleDrawer(true)}
 					>
 						<MenuIcon />
 					</IconButton>
@@ -40,6 +105,15 @@ export default function ButtonAppBar() {
 					<Button color="inherit">Login</Button>
 				</Toolbar>
 			</AppBar>
-		</>
+			<SwipeableDrawer
+				anchor="left"
+				open={openDrawer}
+				onClose={toggleDrawer(false)}
+				onOpen={toggleDrawer(true)}
+			>
+				<Toolbar />
+				<MenuList />
+			</SwipeableDrawer>
+		</div>
 	);
 }
